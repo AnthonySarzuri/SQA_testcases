c3b1acb6cda82ba7baff558f6e3e84e5a122c200
c4l3b <ssa2023593@est.univalle.edu>
2024-12-02 01:56:51 -0400

    terapeuta

---
+++ SQAFinal/test_cases/test_interno/welcomeInterno.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def setup_driver():
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    return driver
+
+def login(driver, email, password):
+
+    driver.get('http://localhost:3000')
+    driver.find_element(By.XPATH, "//div[@class='relative']//input[@type='email']").send_keys(email)
+    driver.find_element(By.XPATH, "//div[@class='relative']//input[@type='password']").send_keys(password)
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+def check_login_success(driver):
+
+    time.sleep(2)  # Espera a que se procese el login
+    try:
+        # Verifica si hay un elemento que indica un inicio de sesión exitoso
+        welcome_element = driver.find_element(By.XPATH, "//h2[contains(text(), 'Bienvenido')]")
+        if welcome_element:
+            print("Login exitoso, navegando a la página de bienvenida...")
+            driver.get('http://localhost:3000/welcome')
+    except Exception as e:
+        print("Login fallido, credenciales no válidas.")
+
+def main():
+    driver = setup_driver()
+
+    try:
+        login(driver, "can@gmail.com", "72005945CA")
+        check_login_success(driver)
+    finally:
+        time.sleep(5)
+        driver.quit()
+        print("Prueba visual completada")
+
+if __name__ == "__main__":
+    main()
